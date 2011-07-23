4adb6f41aca140309d500daa8c2817aedc82b1d1
Espreon <Majora700@gmail.com>
2011-07-23 15:57:25 -0400

    Added a script that generates the runic version from the Latin version with anglat2futhorc.py.

---
+++ utils/make-runic.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+#
+# Script to generate the runic version from the Latin version
+# Requires anglat2futhorc.py in your PATH and
+# a git clone named "wesnoth-ang" in the root of your home directory
+# anglat2futhorc.py can be found here:
+# http://wesnoth-umc-dev.svn.sourceforge.net/viewvc/wesnoth-umc-dev/branches/maintenance-toolchain/anglat2futhorc.py?view=log
+#
+
+td=(
+wesnoth
+wesnoth-anl
+wesnoth-aoi
+wesnoth-did
+wesnoth-dm
+wesnoth-dw
+wesnoth-editor
+wesnoth-ei
+wesnoth-help
+wesnoth-httt
+wesnoth-l
+wesnoth-lib
+wesnoth-low
+wesnoth-manpages
+wesnoth-manual
+wesnoth-multiplayer
+wesnoth-nr
+wesnoth-sof
+wesnoth-sotbe
+wesnoth-tb
+wesnoth-test
+wesnoth-thot
+wesnoth-trow
+wesnoth-tsg
+wesnoth-tutorial
+wesnoth-units
+wesnoth-utbs
+)
+
+for i in "${td[@]}"
+do
+anglat2futhorc.py ~/wesnoth-ang/ang\@latin-po/$i/ang\@latin.po ~/wesnoth-ang/ang-po/$i/ang.po
+done
